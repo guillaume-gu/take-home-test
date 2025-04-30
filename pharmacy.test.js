ffec951796adad0f73cd54304fd814ca3b2d465b
guillaume-gu <guillaumeguerin45@gmail.com>
2025-04-30 16:45:12 +0200

    Add tests

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,31 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
+  it("should add benefit", () => {
+    const drugTest = [new Drug("test", 2, 3)];
+    const drugResult = [new Drug("test", 2, 4)];
+
+    const pharmacy = new Pharmacy(drugTest);
+    pharmacy.addBenefit(pharmacy.drugs[0]);
+
+    expect(pharmacy.drugs).toEqual(drugResult);
+  });
+
+  it("should decrease benefit", () => {
+    const drugTest = [new Drug("test", 2, 3)];
+    const drugResult = [new Drug("test", 2, 2)];
+
+    const pharmacy = new Pharmacy(drugTest);
+    pharmacy.decreaseBenefit(pharmacy.drugs[0]);
+
+    expect(pharmacy.drugs).toEqual(drugResult);
+  });
+
   it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)],
-    );
+    const drugTest = [new Drug("test", 2, 3)];
+    const drugResult = [new Drug("test", 1, 2)];
+    const pharmacy = new Pharmacy(drugTest);
+
+    expect(pharmacy.updateBenefitValue()).toEqual(drugResult);
   });
 });
